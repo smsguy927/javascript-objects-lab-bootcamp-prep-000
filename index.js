37e0a06202541e6fd35d958926e5c601fb39e019
smsguy927 <smsguy927@gmail.com>
2021-03-04 10:03:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function updateObjectWithKeyAndValue(obj, key, val) {
+  // NON-DESTRUCTIVE!
+  let resultObj = Object.assign(resultObj, obj);
+  resultObj[key] = val;
+  return resultObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+
+}
+
+function deleteFromObjectByKey(obj, key) {
+  // NON-DESTRUCTIVE!
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
